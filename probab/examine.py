5002a2c44a480e1036044edf0f6187489136f17b
vign3sh <38295185+vign3sh@users.noreply.github.com>
2021-11-13 01:45:27 -0500

    Agent 7

--- probab/examine.py
+++ probab/examine.py
@@ -62,56 +62,6 @@ def terrain_type(cell, exp_grid, grid, agent):
         exp_grid[x][y].set_pf(0.2)
 
 
-def change_prob(exp_grid, cell, change, pxy, agent) -> Cell:
-    """
-
-    :rtype: Cell
-    """
-    maxProb = 0
-    if agent == 6:
-        maxProb = cell.get_pg()
-    if agent == 7:
-        maxProb = cell.get_pfg()
-
-    maxCell = [cell]
-
-    for i in range(len(exp_grid)):
-        for j in range(len(exp_grid)):
-            if not ((i, j) == cell.get_xy()):
-                pij = exp_grid[i][j].Pg
-                pij = pij + (pij / (1 - pxy)) * change
-                exp_grid[i][j].set_pg(pij)
-
-                if agent == 6:
-                    if pij > maxProb:
-                        maxCell.clear()
-                        maxCell.append(exp_grid[i][j])
-                        maxProb = pij
-
-                    elif pij == maxProb and check_dist(cell, maxCell[-1]) > check_dist(cell, exp_grid[i][j]):
-                        maxCell.clear()
-                        maxCell.append(exp_grid[i][j])
-
-                    elif pij == maxProb and check_dist(cell, maxCell[-1]) == check_dist(cell, exp_grid[i][j]):
-                        maxCell.append(exp_grid[i][j])
-
-                if agent == 7:
-                    pfg = exp_grid[i][j].get_pfg()
-                    if pfg > maxProb:
-                        maxCell.clear()
-                        maxCell.append(exp_grid[i][j])
-                        maxProb = pfg
-
-                    elif pfg == maxProb and check_dist(cell, maxCell[-1]) > check_dist(cell, exp_grid[i][j]):
-                        maxCell.clear()
-                        maxCell.append(exp_grid[i][j])
-
-                    elif pfg == maxProb and check_dist(cell, maxCell[-1]) == check_dist(cell, exp_grid[i][j]):
-                        maxCell.append(exp_grid[i][j])
-
-    return maxCell[random.randint(0, len(maxCell)-1)]
-
-
 # Translate change of probability to all the neighbors
 def update_prob(exp_grid, cell, oldpg, agent):
 
@@ -160,4 +110,53 @@ def update_prob(exp_grid, cell, oldpg, agent):
     return maxCell[random.randint(0, len(maxCell) - 1)]
 
 
+''' Redundant code
+def change_prob(exp_grid, cell, change, pxy, agent) -> Cell:
+    """
 
+    :rtype: Cell
+    """
+    maxProb = 0
+    if agent == 6:
+        maxProb = cell.get_pg()
+    if agent == 7:
+        maxProb = cell.get_pfg()
+
+    maxCell = [cell]
+
+    for i in range(len(exp_grid)):
+        for j in range(len(exp_grid)):
+            if not ((i, j) == cell.get_xy()):
+                pij = exp_grid[i][j].Pg
+                pij = pij + (pij / (1 - pxy)) * change
+                exp_grid[i][j].set_pg(pij)
+
+                if agent == 6:
+                    if pij > maxProb:
+                        maxCell.clear()
+                        maxCell.append(exp_grid[i][j])
+                        maxProb = pij
+
+                    elif pij == maxProb and check_dist(cell, maxCell[-1]) > check_dist(cell, exp_grid[i][j]):
+                        maxCell.clear()
+                        maxCell.append(exp_grid[i][j])
+
+                    elif pij == maxProb and check_dist(cell, maxCell[-1]) == check_dist(cell, exp_grid[i][j]):
+                        maxCell.append(exp_grid[i][j])
+
+                if agent == 7:
+                    pfg = exp_grid[i][j].get_pfg()
+                    if pfg > maxProb:
+                        maxCell.clear()
+                        maxCell.append(exp_grid[i][j])
+                        maxProb = pfg
+
+                    elif pfg == maxProb and check_dist(cell, maxCell[-1]) > check_dist(cell, exp_grid[i][j]):
+                        maxCell.clear()
+                        maxCell.append(exp_grid[i][j])
+
+                    elif pfg == maxProb and check_dist(cell, maxCell[-1]) == check_dist(cell, exp_grid[i][j]):
+                        maxCell.append(exp_grid[i][j])
+
+    return maxCell[random.randint(0, len(maxCell)-1)]
+'''
